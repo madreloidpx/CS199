7a0c6e4074c274bf123227c86ac3e1dd802b2b1f
Aliya Miranda <miranda.aliya@gmail.com>
2020-04-05 16:50:14 +0800

    Added basic bash script to run iter thresholds

---
+++ W-CPMd/getNMIThresholds.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+start=$1
+end=$2
+recursionLimit=$3
+for i in $(seq $start $end)
+do
+    python3 main.py iterateThreshold -gd tests/network$i.dat -rl $recursionLimit -out output/nmiSum$i.txt -tcf tests/community$i.dat
+done
